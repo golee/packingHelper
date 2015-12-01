9d4129e068073d2faece17914cebf30ffecdf31f
golee <golee3@gmail.com>
2015-12-01 11:00:12 +0900

    Initial

---
+++ app/src/main/java/project/hci/packinghelper/InsertActivity.java
@@ -0,0 +1,14 @@
+package project.hci.packinghelper;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+}
